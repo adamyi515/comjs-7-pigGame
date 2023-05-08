8467ef2bbb728abf84c888f113c903bff955ab76
Adam Yi <adamyi515@gmail.com>
2023-05-08 17:03:52 -0500

    Complete logic for roll dice button and hold button

--- script.js
+++ script.js
@@ -4,6 +4,7 @@
 let currentScore = 0;
 let activePlayer = 0;
 const totalScores = [0, 0];
+let isPlaying = true;
 
 // Selecting Elements =======================================================================
 const lblCurScore0EL = document.querySelector('#current--0');
@@ -25,30 +26,45 @@ init() // Initialize the game.
 
 // Event Handler =============================================================================
 btnRoll.addEventListener('click', function(){
-    // 1. Generate a number between 1-6.
-    const number = rollDice();
+    if(isPlaying){
+        // 1. Generate a number between 1-6.
+        const number = rollDice();
 
-    // 2. Display the dice.
-    imgDice.classList.remove('hidden');
-    imgDice.src = `dice-${number}.png`;
+        // 2. Display the dice.
+        imgDice.classList.remove('hidden');
+        imgDice.src = `dice-${number}.png`;
 
-    // 3. Check if the number is 1: if true, switch to next player.
-    // If number 2-6, add number to current score.
-    if(number !== 1){
-        currentScore += number;
-        document.querySelector(`#current--${activePlayer}`).textContent = currentScore;
-    } else {
-        // Switch to next player b/c number == 1.
-        currentScore = 0;
-        document.querySelector(`#current--${activePlayer}`).textContent = currentScore;
-        activePlayer = activePlayer === 0 ? 1 : 0;
-
-        // Change CSS to indicate which player is active.
-        player0EL.classList.toggle('player--active');
-        player1EL.classList.toggle('player--active');
+        // 3. Check if the number is 1: if true, switch to next player.
+        // If number 2-6, add number to current score.
+        if(number !== 1){
+            currentScore += number;
+            document.querySelector(`#current--${activePlayer}`).textContent = currentScore;
+        } else {
+            // Switch to next player b/c number == 1.
+            switchPlayer();
+        }
     }
-
 });
+
+
+btnHold.addEventListener('click', function(){
+    if(isPlaying){
+        // 1. Add current score to total score of current player.
+        totalScores[activePlayer] += currentScore;
+        document.querySelector(`#score--${activePlayer}`).textContent = totalScores[activePlayer];
+
+        // 2. Check to see if the total score is >= to 100. If so, the player wins.
+        if(totalScores[activePlayer] >= 20){
+            // Player wins. End the game.
+            document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+            isPlaying = false;
+        } else {
+            // 3. If total score is not >= 100, then switch player.
+            switchPlayer();
+        }
+    }
+});
+
 
 
 
@@ -69,4 +85,14 @@ function init(){
 // Generates number between 1-6
 function rollDice(){
     return Math.floor(Math.random() * 6) + 1;
+}
+
+function switchPlayer() {
+    currentScore = 0;
+    document.querySelector(`#current--${activePlayer}`).textContent = currentScore;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+
+     // Change CSS to indicate which player is active.
+     player0EL.classList.toggle('player--active');
+     player1EL.classList.toggle('player--active');
 }
